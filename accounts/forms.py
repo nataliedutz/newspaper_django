06e17e9a963f72882093a133414214a6dfef62db
nataliedutz <natalie.dutz@dci-student.org>
2024-02-07 13:25:53 +0100

    Initial commit

---
+++ accounts/forms.py
@@ -0,0 +1,18 @@
+# accounts/forms.py
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        # fields = UserCreationForm.Meta.fields + ("age",)
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
